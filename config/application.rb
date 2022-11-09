8b69789f4379f0730fb48270f65391c8daf21d6d
Brian Chang <brian.chang@coconutsoftware.com>
2022-11-08 21:57:07 -0800

    Add 'factory' suffix to generated factories

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module Blogster
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.factory_bot suffix: "factory"
+    end
   end
 end
