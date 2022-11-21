7950b864f49d142bd7dfcf5bb568061fff4ad861
Brian Chang <changbhc@gmail.com>
2022-11-20 17:25:36 -0800

    Fix rubocop suggestions

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,24 +1,22 @@
 class HealthController < ApplicationController
   def database
-    begin
-      ActiveRecord::Base.establish_connection
-      ActiveRecord::Base.connection
-      if ActiveRecord::Base.connected?
-        render json: {
-          success: true,
-          message: 'Database connected'
-        }
-      else
-        render json: {
-          success: false,
-          message: 'Not connected to database'
-        }
-      end
-    rescue => e
+    ActiveRecord::Base.establish_connection
+    ActiveRecord::Base.connection
+    if ActiveRecord::Base.connected?
+      render json: {
+        success: true,
+        message: 'Database connected'
+      }, status: :ok
+    else
       render json: {
         success: false,
-        message: e
-      }
+        message: 'Not connected to database'
+      }, status: :not_acceptable
     end
+  rescue => e
+    render json: {
+      success: false,
+      message: e
+    }, status: :internal_server_error
   end
 end
