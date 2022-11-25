911140a0bd03c0fd6836a0dc453508a5385fcc01
Brian Chang <brian.chang@coconutsoftware.com>
2022-11-24 16:39:50 -0800

    Add slugs to Articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_033538) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_003049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_033538) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "published_date"
+    t.string "slug"
   end
 
 end
