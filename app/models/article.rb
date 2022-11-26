00a2d67c429a0eb1af3628a0a9f1cedc0b5d4766
Brian Chang <brian.chang@coconutsoftware.com>
2022-11-25 16:31:03 -0800

    Set slugs before save

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  before_save :set_slug
+
+  def to_param
+    return nil unless persisted?
+
+    [id, slug].join('-')
+  end
+
+  def set_slug
+    self.slug = title.parameterize
+  end
 end
