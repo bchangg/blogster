68a4f074b86d4bd84edf3814dd9ff5cc3d3b8a69
Brian Chang <changbhc@gmail.com>
2023-10-27 17:41:24 -0700

    185867811: Clean up test file with rubocop rules

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,26 +2,26 @@
 
 require 'rails_helper'
 
-RSpec.describe Article, type: :model do
+RSpec.describe Article do
   describe 'is valid when' do
     it 'title is present' do
-      article = Article.new(body: 'body')
-      expect(article).to_not be_valid
+      article = described_class.new(body: 'body')
+      expect(article).not_to be_valid
 
       article.title = 'title'
       expect(article).to be_valid
     end
 
     it 'body is present' do
-      article = Article.new(title: 'title')
-      expect(article).to_not be_valid
+      article = described_class.new(title: 'title')
+      expect(article).not_to be_valid
 
       article.body = 'body'
       expect(article).to be_valid
     end
 
     it 'title and body are present' do
-      article = Article.new(
+      article = described_class.new(
         title: 'title',
         body: 'body'
       )
