beaa014c701013ac07b201fc6e3658acaa4dc496
Brian Chang <changbhc@gmail.com>
2024-01-06 21:31:42 -0800

    186758702: Add POC router and temporary components for each route

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import GreenTea from "./GreenTea";
+import BlackTea from "./BlackTea";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return <h1>Hello from React!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="green-tea" element={<GreenTea />} />
+        <Route path="black-tea" element={<BlackTea />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ app/javascript/components/BlackTea.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BlackTea() {
+  return <h1>Black Tea</h1>;
+}
