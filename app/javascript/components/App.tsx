4471195c1b7287bf5234b6022809b89975390e41
Brian Chang <changbhc@gmail.com>
2024-02-05 10:12:21 -0800

    186735599: Move the BrowserRouter to the app entrypoint
    We might want to use MemoryRouter for testing instead, this will
    isolate the App component so it'll be easier when testing.

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import GreenTea from "./GreenTea";
 import BlackTea from "./BlackTea";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="green-tea" element={<GreenTea />} />
-        <Route path="black-tea" element={<BlackTea />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="green-tea" element={<GreenTea />} />
+      <Route path="black-tea" element={<BlackTea />} />
+    </Routes>
   );
 }
